d05736d974c12db4c592e27f34710cb64ab0a863
thienleanh <thien.lah@gmail.com>
2019-10-24 08:58:18 +0700

    * Remove userDetailService

--- demo/src/main/java/com/example/demo/services/UserDetailsServiceImpl.java
+++ demo/src/main/java/com/example/demo/services/UserDetailsServiceImpl.java
@@ -18,33 +18,38 @@ import java.util.List;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
-    private AppUserDAO appUserDAO;
-
-    @Autowired
-    private AppRoleDAO appRoleDAO;
+//    @Autowired
+//    private AppUserDAO appUserDAO;
+//
+//    @Autowired
+//    private AppRoleDAO appRoleDAO;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+//
+//        AppUser appUser = this.appUserDAO.findUserAccount(userName);
+//
+//        if (appUser == null) {
+//            System.out.println("User not found! " + userName);
+//            throw new UsernameNotFoundException("User " + userName + " was not found in the database");
+//        }
+//
+//        List<String> roleNames = this.appRoleDAO.getRoleNames(appUser.getUserId());
+//
+//        List<GrantedAuthority> grantList = new ArrayList<>();
+//        if (roleNames != null) {
+//            for (String role : roleNames) {
+//                GrantedAuthority authority = new SimpleGrantedAuthority(role);
+//                grantList.add(authority);
+//            }
+//        }
+//
+//        return new User(appUser.getUserName(), appUser.getEncryptedPassword(), grantList);
+//    }
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-
-        AppUser appUser = this.appUserDAO.findUserAccount(userName);
-
-        if (appUser == null) {
-            System.out.println("User not found! " + userName);
-            throw new UsernameNotFoundException("User " + userName + " was not found in the database");
-        }
-
-        List<String> roleNames = this.appRoleDAO.getRoleNames(appUser.getUserId());
-
-        List<GrantedAuthority> grantList = new ArrayList<>();
-        if (roleNames != null) {
-            for (String role : roleNames) {
-                GrantedAuthority authority = new SimpleGrantedAuthority(role);
-                grantList.add(authority);
-            }
-        }
-
-        return new User(appUser.getUserName(), appUser.getEncryptedPassword(), grantList);
+        return null;
     }
 
 }
